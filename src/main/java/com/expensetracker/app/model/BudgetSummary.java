3507e76fc5dba0e8025de20e983d167c256c2aeb
teledi_makhafola <lkgahliso03@gmail.com>
2023-07-24 11:33:44 +0200

    added expense and budgetSummary models

--- src/main/java/com/expensetracker/app/model/BudgetSummary.java
+++ src/main/java/com/expensetracker/app/model/BudgetSummary.java
@@ -1,4 +1,30 @@
 package com.expensetracker.app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "budget_summary")
 public class BudgetSummary {
+    @Column(name = "total_budget")
+    private double totalBudget;
+
+    @Column(name = "total_expenses")
+    private double totalExpenses;
+
+    @Column(name = "savings")
+    private double savings;
+
+    @Column(name = "is_under_budget")
+    private boolean isUnderBudget;
 }
--- src/main/java/com/expensetracker/app/model/Expense.java
+++ src/main/java/com/expensetracker/app/model/Expense.java
@@ -1,4 +1,31 @@
 package com.expensetracker.app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "expense")
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private LocalDate date;
 }
