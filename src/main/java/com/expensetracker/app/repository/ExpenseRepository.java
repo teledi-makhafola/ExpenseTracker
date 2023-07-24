2c941b75eabdcd0ee41ddd2e324ea966e8367f8a
teledi_makhafola <lkgahliso03@gmail.com>
2023-07-24 12:05:58 +0200

    added business logic and updated the repo

--- src/main/java/com/expensetracker/app/repository/ExpenseRepository.java
+++ src/main/java/com/expensetracker/app/repository/ExpenseRepository.java
@@ -1,4 +1,16 @@
 package com.expensetracker.app.repository;
 
+import com.expensetracker.app.model.Expense;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ExpenseRepository {
+    void deleteById(Long id);
+
+    Expense save(Expense expense);
+
+    Optional<Expense> findById(Long id);
+
+    List<Expense> findAll();
 }
--- src/main/java/com/expensetracker/app/service/ExpenseService.java
+++ src/main/java/com/expensetracker/app/service/ExpenseService.java
@@ -1,4 +1,47 @@
 package com.expensetracker.app.service;
 
+import com.expensetracker.app.model.BudgetSummary;
+import com.expensetracker.app.model.Expense;
+import com.expensetracker.app.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+
+    @Autowired
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public Expense getExpenseById(Long id) {
+        return expenseRepository.findById(id).orElse(null);
+    }
+
+    public Expense saveExpense(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public void deleteExpense(Long id) {
+        expenseRepository.deleteById(id);
+    }
+
+    public BudgetSummary calculateBudgetSummary(double totalBudget) {
+        List<Expense> allExpenses = getAllExpenses();
+
+        double totalExpenses = allExpenses.stream()
+                .mapToDouble(Expense::getAmount)
+                .sum();
+
+        double savings = totalBudget - totalExpenses;
+        boolean isUnderBudget = savings >= 0;
+
+        BudgetSummary budgetSummary = new BudgetSummary(totalBudget, totalExpenses, savings, isUnderBudget);
+        return budgetSummary;
+    }
 }
