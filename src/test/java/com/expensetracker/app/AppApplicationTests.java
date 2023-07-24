3c18d5a4f6c1f4b3945f9d3b861e04e7f2ca954a
teledi_makhafola <lkgahliso03@gmail.com>
2023-07-24 11:08:56 +0200

    start of the project

---
+++ src/test/java/com/expensetracker/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
